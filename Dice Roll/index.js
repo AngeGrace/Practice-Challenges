ff73bc9b41ba52338725b092e055a5838d04e874
Angela Grace <angelamegz@gmail.com>
2024-06-11 21:43:55 +0200

    Roll dice button added

--- Dice Roll/index.js
+++ Dice Roll/index.js
@@ -5,3 +5,8 @@ document.querySelectorAll("img")[0].setAttribute("src" , randomDiceImage);
 var randomNumber2 = Math.floor(Math.random() * 6)+ 1;
 var randomDiceImage2 = "images/" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src" , randomDiceImage2);
+
+const refreshButton = document.getElementById("refresh-btn");
+refreshButton.addEventListener("click", function() {
+    location.reload();
+  });
