a54f7fe92b661d6b7fe3ab04f30400637267719a
Angela Grace <angelamegz@gmail.com>
2024-06-11 16:11:01 +0200

    Add dice app (customised) and drum kit app

---
+++ Dice Roll/index.js
@@ -0,0 +1,7 @@
+var randomNumber1 = Math.floor(Math.random() * 6)+ 1;
+var randomDiceImage = "images/" + randomNumber1 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src" , randomDiceImage);
+       
+var randomNumber2 = Math.floor(Math.random() * 6)+ 1;
+var randomDiceImage2 = "images/" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src" , randomDiceImage2);
